74d3a6d2e52824e72ab927e6adc961e9b5ff648b
Darren Wayn <darrenwayn98@gmail.com>
2022-09-21 14:25:02 +0700

    added search filter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Component } from "react";
-import "./App.css";
 
 class App extends Component {
   constructor() {
@@ -7,16 +6,20 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
+    console.log("constructor");
   }
 
   componentDidMount() {
+    console.log("component did mount");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
         this.setState(
           () => {
             return { monsters: users };
+            // test
           },
           () => {
             console.log(this.state);
@@ -26,36 +29,38 @@ class App extends Component {
   }
 
   render() {
+    console.log("render");
+    const filteredMonster = this.state.monsters.filter((monster) =>
+      monster.name.toLocaleLowerCase().includes(this.state.searchField)
+    );
+
+    const handleChange = (e) => {
+      const searchField = e.target.value.toLocaleLowerCase();
+      this.setState(() => {
+        return { searchField };
+      });
+    };
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input
+          clasName="search-box"
+          type="search"
+          placeholder="search monster"
+          onChange={handleChange}
+        />
+        {filteredMonster.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
           );
         })}
+
+        {/* <button onClick={} */}
       </div>
     );
   }
 }
 
 export default App;
-
-// {/* <button
-//   onClick={() => {
-//     this.setState({
-//       name: { firstName: "Andrei", lastName: "Luan" },
-//     });
-//     this.setState(
-//       (state, props) => {
-//         return { name: { firstName: "Andrei", lastName: "Luan" } };
-//       },
-//       () => {
-//         console.log(this.state);
-//       }
-//     );
-//   }}
-// >
-//   Change Name
-// </button>; */}
